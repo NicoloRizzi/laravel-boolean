8cc029d77891ebd46d052be4227cd62b07e5555d
Nicolo <nicolorizzi1996@gmail.com>
2020-06-11 16:16:55 +0200

    init markup in a single pages

--- BooleanCareers/resources/views/static-pages/index.blade.php
+++ BooleanCareers/resources/views/static-pages/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.main')
 
 @section('main-content')
-
+    <div class="jumbotron">
+        <div class="jumbo-text">
+            <h2>Diventa Sviluppatore Web</h2>
+            <p><b>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</b></p>
+            <ul>
+                <li><b>6 mesi</b> di corso intensivo online in diretta</li>
+                <li><b>Nessuna competenza</b> di programmazione richiesta</li>
+                <li>Siamo certi del tuo successo, altrimenti <b>ti rimborsiamo</b></li>
+            </ul>
+        </div>
+        <div class="jumbo img">
+            <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="Impara da casa tua">
+        </div>
+    </div>
 @endsection
