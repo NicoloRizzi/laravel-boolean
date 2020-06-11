361158066c13b65b5b7ce898e88afff2b8737c4a
Nicolo <nicolorizzi1996@gmail.com>
2020-06-11 19:07:38 +0200

    styling cards student

--- BooleanCareers/resources/views/students/index.blade.php
+++ BooleanCareers/resources/views/students/index.blade.php
@@ -6,7 +6,7 @@
     @foreach($students as $student)
     <a href="#" class="student">
         <header>
-        <img src="{{ $student['img'] }}" alt="{{ $student['nome'] }}">
+        <img class="img-student" src="{{ $student['img'] }}" alt="{{ $student['nome'] }}">
             <div class="info">
             <h3>{{ $student['nome'] }} ({{ $student['eta']}} anni)</h3>
             <h4>
