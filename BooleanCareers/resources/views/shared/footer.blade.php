15d5d47443bfdb8ae38543d16554df14d5889577
Nicolo <nicolorizzi1996@gmail.com>
2020-06-11 15:46:46 +0200

    include a portion of code in a different file

---
+++ BooleanCareers/resources/views/shared/footer.blade.php
@@ -0,0 +1,19 @@
+    <footer class="main-footer">
+        <nav class="main-footer">
+            <div class="footer-brand">-->LOGO BOOLEAN<---</div>
+            <ul>
+            {{-- INSERISCI LEZIONE GRATUITA STATIC PAGE - CONTROLLER + ROUTE --}}
+                <li>
+                    <a href="{{ route('static-page.home') }}">home</a>
+                </li>
+                    <li>
+                        <a href="{{ route('static-page.faq') }}">domande frequenti</a>
+                    </li>
+                    <li>
+                        <a href="{{ route('static-page.privacy') }}">privacy</a>
+                    </li>
+            </ul>
+        </nav>
+    </footer>
+    </body>
+</html>
---
+++ BooleanCareers/resources/views/shared/header.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel Boolean</title>
+</head>
+<body>
+    <header class="main-header">
+        <nav class="navbar">
+            <a href="" class="navbar-brand">-->LOGO BOOLEAN<---</a>
+            <ul>
+                <li><a href="{{ route ('static-page.home') }}">home</a></li>
+                <li><a href="">corso</a></li>
+                <li><a href="{{ route ('student.index') }}">dopo il corso</a></li>
+                <li><a href="">lezione gratuita</a></li>
+                <li><a href="">assumi i nostri studenti</a></li>
+                <li><a href="">candidati ora</a></li>
+            </ul>
+        </nav>
+    </header>
