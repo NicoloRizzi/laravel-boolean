bf0228198a0189266b42e527bd00be218b523a24
Nicolo <nicolorizzi1996@gmail.com>
2020-06-12 16:26:44 +0200

    CREATED A NEW ROUTE AND CONTROLLER FOR API

---
+++ BooleanCareers/app/Http/Controllers/Api/StudentController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    /**
+     * STUDENT FILTERED BY GENDER ENDPOINT
+     */
+    public function gender () {
+
+    }
+
+}
