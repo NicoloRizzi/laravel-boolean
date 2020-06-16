f7da4553118f4ab2cb3e610f9495e7af3840ed79
Nicolo <nicolorizzi1996@gmail.com>
2020-06-16 09:27:31 +0200

    STYLING JUMBOTRON

--- BooleanCareers/resources/views/static-pages/index.blade.php
+++ BooleanCareers/resources/views/static-pages/index.blade.php
@@ -3,16 +3,16 @@
 @section('main-content')
     <div class="jumbotron">
         <div class="jumbo-text">
-            <h1>Diventa <b>Sviluppatore Web</b></h1>
-            <p><b>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</b></p>
+            <h1>Diventa<br><b>Sviluppatore <br>Web</b></h1>
+            <h4>Trasformiamo la tua passione in una carriera. Se non trovi lavoro, ti rimborsiamo.</h4>
             <ul>
                 <li><b>6 mesi</b> di corso intensivo online in diretta</li>
                 <li><b>Nessuna competenza</b> di programmazione richiesta</li>
                 <li>Siamo certi del tuo successo, altrimenti <b>ti rimborsiamo</b></li>
             </ul>
         </div>
-        <div class="jumbo img">
-            <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="Impara da casa tua">
+        <div class="jumbo-img">
+            <img src="https://www.boolean.careers/images/homepage/pc-black-gif.gif" alt="Impara da casa tua" class="pc-gif">
         </div>
     </div>
 @endsection
